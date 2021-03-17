76e70c3675ef7d59c0b3282aabe4b429cddffc7d
Kritarth <kritarth@plivo.com>
2021-03-17 18:59:32 +0530

    changes

--- lib/chatur/console.ex
+++ lib/chatur/console.ex
@@ -1,5 +1,24 @@
+defmodule CrossPlatform.Console do
+  require Logger
+  defmacro __using__(_) do
+    case :os.type do
+      {:win32, _} ->
+        quote do
+          @exec_file "Z:\say.cfg"
+        end
+      {:unix, :linux} ->
+        quote do
+          @exec_file "/tmp/say.cfg"
+        end
+      ost ->
+        Logger.error("Unsupported platform #{inspect ost}")
+        :err
+    end
+  end
+end
+
 defmodule Console do
-  @exec_file "/tmp/say.cfg"
+  use CrossPlatform.Console
   @exec_key "p"
 
   def execute(command) do
